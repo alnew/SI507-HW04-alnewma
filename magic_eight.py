9ca910825472afbb51b468fb58465c2a9d398729
Amy Newman <amynewman2@gmail.com>
2018-01-29 17:38:32 -0500

    added magic_q while loop

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,10 @@
 def user_input():
     m = input("What is your question")
     return m
+
+magic_q = " "
+
+while magic_q != "quit":
+    magic_q = user_input()
+    if magic_q[-1] == "?":
+        print (“I’m sorry, I can only answer questions.”)
